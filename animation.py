5de033105c898f0476d484bee1bedfe36fac4945
nikkopang <57596275+nikkopang@users.noreply.github.com>
2021-06-27 04:05:24 +0800

    Add files via upload

---
+++ animation.py
@@ -0,0 +1,15 @@
+import os, time
+
+os.system('cls')
+fileNames = ["waterfall1.txt", "waterfall2.txt", "waterfall3.txt"]
+frames = []
+
+for name in fileNames:
+	with open(name, "r", encoding="utf8") as f:
+		frames.append(f.readlines())
+		
+for i in range(10):
+	for frame in frames:
+		print("".join(frame))
+		time.sleep(0.1)
+		os.system('cls')
